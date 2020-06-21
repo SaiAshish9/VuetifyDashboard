0fe400dc8c9403eedacdb6cbeb290f032acf8105
SaiAshish9 <saiashish3760@gmail.com>
2020-06-22 03:06:47 +0530

    added customized svg

---
+++ stuff/font.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p class="red white--text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem fuga,
+      adipisci nostrum sit animi officia repellat voluptatibus consectetur
+      provident, veniam saepe quibusdam ducimus omnis esse aperiam totam
+      eligendi libero tempore.
+    </p>
+    <p class="red lighten-4 pink--text text-darken--4">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure praesentium
+      impedit officiis ab blanditiis expedita animi possimus minima maiores
+      ratione ad quibusdam modi, obcaecati, aut velit quo nobis vitae inventore.
+    </p>
+    <h1 class="display-1">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae
+      tempore labore, ab explicabo tempora aut aspernatur ipsa delectus tenetur
+      atque reiciendis ratione repellat architecto unde nobis enim quaerat quia
+      exercitationem!
+    </h1>
+    <p class="headline">
+      headline
+    </p>
+    <p class="subheading font-weight-italic gray--text text-lighten--4">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero debitis,
+      officiis, temporibus nobis officia voluptas magni delectus tempora sunt,
+      quo perferendis deleniti. Ipsa rem harum eveniet assumenda ducimus numquam
+      nemo.
+    </p>
+    <p class="caption text-uppercase">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat dolorem
+      illo maxime tempore nostrum odio labore sit accusamus aut qui provident
+      voluptatem error dicta, nulla quo ab. Vitae, rem voluptatem?
+    </p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+};
+</script>
